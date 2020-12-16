7e1ca968897d45374db52028192fd649aabd489c
Thomas Maier <thomas.maier@gmx.com>
2020-12-16 14:09:15 +0100

    Weather Feature - improvements and updates

--- infodisplay/server/weatherServer/weather_server.py
+++ infodisplay/server/weatherServer/weather_server.py
@@ -10,8 +10,6 @@ from infodisplay.server.weatherServer.weather_provider import AccuWeather
 from infodisplay.utils.file_utils import get_dict_from_json_file, mqtt_send_json_file
 from infodisplay.utils.mqtt_client import MqttClient
 
-INTERVAL = 60 * 60  # time in seconds -> 1h
-
 
 class WeatherServer(threading.Thread):
 
@@ -23,6 +21,8 @@ class WeatherServer(threading.Thread):
         self.mqtt_client = MqttClient(ConfigWeather.MQTT_HOST,
                                       ConfigWeather.MQTT_BASE_TOPIC,
                                       ConfigWeather.MQTT_TOPIC)
+        self.update_interval = ConfigWeatherServer.UPDATE_INTERVAL_CURRENT
+        self.forecast_counter = ConfigWeatherServer.UPDATE_INTERVAL_FORECAST
 
     def run(self):
         while self.run_event.is_set():
@@ -31,17 +31,23 @@ class WeatherServer(threading.Thread):
                 # device = IDevicePeripheral(self.mac_address, "iGrillv2")
                 accu_weather = AccuWeather()
                 while True:
+                    logger.info(f'self.forecast_counter: {self.forecast_counter}')
+                    accu_weather.update_current_weather()
                     accu_weather.update_weather_data()
-                    logger.info(f'--> accu_weather.update_weather_data()')
-                    # push mqtt
+                    logger.info(f'--> accu_weather.update_current_weather()')
                     current = mqtt_send_json_file(ConfigWeatherServer.JSON_CURRENT_WEATHER)
                     self.mqtt_client.publish("current", current)
 
-                    forecast = mqtt_send_json_file(ConfigWeatherServer.JSON_WEATHER_FORECAST)
-                    self.mqtt_client.publish("forecast", forecast)
-                    time.sleep(INTERVAL)
+                    if self.forecast_counter == ConfigWeatherServer.UPDATE_INTERVAL_FORECAST:
+                        self.forecast_counter = 0
+                        forecast = mqtt_send_json_file(ConfigWeatherServer.JSON_WEATHER_FORECAST)
+                        self.mqtt_client.publish("forecast", forecast)
+                        logger.info(f'--> accu_weather.update_weather_forecast()')
+
+                    self.forecast_counter += 1
+                    time.sleep(self.update_interval)
             except Exception as e:
                 logger.error(f'error occured: {e}')
-                logger.error(f'sleeping for {INTERVAL} seconds before retrying')
-                time.sleep(INTERVAL)
+                logger.error(f'sleeping for {self.update_interval} seconds before retrying')
+                time.sleep(self.update_interval)
         logger.info(f'"Thread exiting')
--- infodisplay/ui/weather/weather.py
+++ infodisplay/ui/weather/weather.py
@@ -6,19 +6,16 @@ from PyQt5.QtWidgets import QWidget, QLabel, QHBoxLayout, QVBoxLayout
 from loguru import logger
 import paho.mqtt.client as mqttClient
 
+from configs.config_weather import ConfigWeather
 from infodisplay.utils.file_utils import dump_dict2json_file, save_jsondata_2_jsonfile
-
-CLIENT_ID = 'DELETE_999'
-PORT = 1883
-HOST = 'rp3'
 
 
 class WeatherDataThread(QThread):
-    signal_msg = pyqtSignal(str, str)
+    signal_msg = pyqtSignal(str)
 
     def __init__(self):
         QThread.__init__(self)
-        self.client = mqttClient.Client(CLIENT_ID)
+        self.client = mqttClient.Client(ConfigWeather.MQTT_CLIENT_ID)
 
     def on_connect_cb(self, client, userdata, flags, rc):
         logger.info(f'connected to broker with result code: {str(rc)}')
@@ -28,13 +25,26 @@ class WeatherDataThread(QThread):
     def on_message_cb(self, client, userdata, msg):
         logger.info(f'message received -> topic: {msg.topic}, payload: {str(msg.payload)}')
         payload = (msg.payload.decode('utf-8'))
-        self.signal_msg.emit(msg.topic, str(payload))
+        self._save_received_data_to_file(msg.topic, msg.payload)
+        self.signal_msg.emit(msg.topic)
+
+    def _save_received_data_to_file(self, topic, payload):
+        if topic == ConfigWeather.MQTT_TOPIC_CURRENT:
+            filename = ConfigWeather.MQTT_TOPIC_CURRENT
+        elif topic == ConfigWeather.MQTT_TOPIC_FORECAST:
+            filename = ConfigWeather.JSON_WEATHER_FORECAST
+        else:
+            logger.error(f'unknown topic: {topic}')
+            return
+        d = json.loads(payload)
+        logger.info(f'd, type: {type(d)}')
+        save_jsondata_2_jsonfile(filename, d)
 
     def run(self):
-        logger.info(f'WeatherDataThread')
+        logger.info(f'WeatherDataThread started')
         self.client.on_connect = self.on_connect_cb
         self.client.on_message = self.on_message_cb
-        self.client.connect(HOST, PORT)
+        self.client.connect(ConfigWeather.MQTT_HOST, ConfigWeather.MQTT_PORT)
         self.client.loop_forever()
 
 
@@ -47,7 +57,8 @@ class Weather(QWidget):
         self.thread = WeatherDataThread()
         self.start_threads()
 
-        # self.refresh_timer = QTimer()
+        self._update_current_weather()
+        self._update_weather_forecast()
 
         logger.info(f'with: {parent.width()}, height: {parent.height()}')
         self.testUI()
@@ -56,22 +67,26 @@ class Weather(QWidget):
         self.thread.signal_msg.connect(self.message_received_cb)
         self.thread.start()
 
-    def message_received_cb(self, topic, payload):
-        logger.info(f'message_received_cb t: {topic}, p:{payload}')
-        if topic == 'infoscreens/weather/current':
-            filenanme = 'infodisplay/ui/weather/res/current_weather.json'
-        elif topic == 'infoscreens/weather/forecast':
-            filenanme = 'infodisplay/ui/weather/res/weather_forecast.json'
+    def message_received_cb(self, topic):
+        logger.info(f'message_received_cb t: {topic}')
+        if topic == ConfigWeather.MQTT_TOPIC_CURRENT:
+            self._update_current_weather()
+        elif topic == ConfigWeather.MQTT_TOPIC_FORECAST:
+            self._update_weather_forecast()
         else:
             logger.error(f'unknown topic: {topic}')
             return
-        d = json.loads(payload)
-        logger.info(f'd, type: {type(d)}')
-        save_jsondata_2_jsonfile(filenanme, d)
 
+    def _update_current_weather(self):
+        filename = ConfigWeather.JSON_CURRENT_WEATHER
+        logger.info(f'_update_current_weather')
+
+    def _update_weather_forecast(self):
+        filename = ConfigWeather.JSON_WEATHER_FORECAST
+        logger.info(f'_update_weather_forecast')
 
     def testUI(self):
         test = QLabel("Laber", self)
-        test.move(100, 0)
+        test.move(0, 0)
         test.setText("alalalal\nkaksksk\tndndnd")
         self.show()
