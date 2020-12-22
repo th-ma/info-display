e49ffce4be5bcd46a217f8ccf93445d161a5a3ae
Thomas Maier <thomas.maier@gmx.com>
2020-12-22 11:19:38 +0100

    Weather Feature - clean-up outdoor weather

--- infodisplay/ui/weather/weather.py
+++ infodisplay/ui/weather/weather.py
@@ -5,9 +5,8 @@ from PyQt5.QtWidgets import QWidget, QLabel, QFrame, QHBoxLayout, QVBoxLayout
 from loguru import logger
 
 from configs.config_weather import ConfigWeather, lblHeader, lblText
-from infodisplay.ui.weather.poc_layout import WeatherOutdooor
-from infodisplay.ui.weather.weatherOutdoor import WeatherUiOutdoor
-from infodisplay.ui.weather.weather_outdoor import WeatherOutdoor
+from infodisplay.ui.weather.weather_outdoor import WeatherOutdooor
+
 from infodisplay.ui.weather.weather_thread import WeatherDataThread
 from infodisplay.utils.file_utils import get_dict_from_json_file
 
@@ -21,9 +20,6 @@ class Weather(QWidget):
         self.thread = WeatherDataThread()
         self.start_threads()
 
-        self._update_current_weather()
-        self._update_weather_forecast()
-
         logger.info(f'with: {parent.width()}, height: {parent.height()}')
         # read data
         self.forecast = get_dict_from_json_file(ConfigWeather.JSON_WEATHER_FORECAST)
@@ -35,104 +31,9 @@ class Weather(QWidget):
         self.region_indoor = self.createRegionIndoor()
         # update UI
         self.outdoor = WeatherOutdooor(self.region_outdoor)
-        self.outdoor.updateData(self.current)
-        #self.outdoor = WeatherOutdoor(self.region_outdoor)
-        #self.outdoor.set_data(self.current)
 
-        #self.w = WeatherUiOutdoor(self.region_outdoor)
-    #    self.updateOutdoorUI()
-        #self.setOutdoor()
-      #  self.updateForecastUI()
-     #   self.updateIndoorUI()
-    #    self.updateTimeDateUI()
-
-    def setOutdoor(self):
-        # collect data
-        header_txt = f'Außen'
-        current_temp_txt = str(self.current[0]['WeatherText']) + '°C'
-        feel_temperature_txt = str(self.current[0]['RealFeelTemperature']['Metric']['Value']) + '°C'
-        humidity_txt = self.current[0]['RelativeHumidity']
-        weather_text_txt = self.current[0]['WeatherText']
-        weather_icon_id = self.current[0]['WeatherIcon']
-        weather_icon = self._get_weather_icon(weather_icon_id, self.region_outdoor)
-        wind_speed_txt = str(self.current[0]['Wind']['Speed']['Metric']['Value']) + ' ' + \
-                             str(self.current[0]['Wind']['Speed']['Metric']['Unit'])
-        wind_direction = str(self.current[0]['Wind']['Direction']['Degrees']) + '° aus ' + \
-                                 str(self.current[0]['Wind']['Direction']['Localized'])
-
-        vbox = QVBoxLayout(self.region_outdoor)
-        h_box = QHBoxLayout()
-        t = lblText('Outdoor')
-        t.setFixedHeight(22)
-        h_box.addWidget(t)
-
-        # current weather
-        cw_box = QHBoxLayout()
-        cw_box.addWidget(weather_icon)
-        cw_box.addStretch(1)
-        cw_box.addWidget(lblText(weather_text_txt))
-        cw_box.addStretch(2)
-        cw_box.setAlignment(Qt.AlignTop)
-        h_box.setAlignment(Qt.AlignCenter)
-        #
-        vbox.addLayout(cw_box)
-        vbox.addLayout(h_box)
-
-
-
-
-
-
-    def updateOutdoorUI(self):
-        logger.info(f'updateOutdoorUI')
-        title = lblHeader("Outdoor", self.region_outdoor)
-        x = self._get_center_x(title, self.region_outdoor.width())
-        title.move(x, 0)
-        # prepare data
-        current_temperature = lblText(str(self.current[0]['Temperature']['Metric']['Value']) + '°C',
-                                      self.region_outdoor)
-        feel_temperature = lblText(str(self.current[0]['RealFeelTemperature']['Metric']['Value']) + '°C',
-                                   self.region_outdoor)
-        humidity = lblText(str(self.current[0]['RelativeHumidity']) + ' %', self.region_outdoor)
-        weather_text = lblText(self.current[0]['WeatherText'], self.region_outdoor)
-        weather_icon_id = self.current[0]['WeatherIcon']
-        weather_icon = self._get_weather_icon(weather_icon_id,self.region_outdoor)
-        wind_speed = lblText(str(self.current[0]['Wind']['Speed']['Metric']['Value']) + ' ' +
-                             str(self.current[0]['Wind']['Speed']['Metric']['Unit']), self.region_outdoor)
-        wind_direction = lblText(str(self.current[0]['Wind']['Direction']['Degrees']) + '° aus ' +
-                                 str(self.current[0]['Wind']['Direction']['Localized']),
-                                 self.region_outdoor)
-
-        LINE = 40
-        weather_icon.move(0, LINE)
-        weather_text.move(80, LINE)
-        logger.info(weather_icon.width())
-        current_temperature.move(self._get_center_x(current_temperature, self.region_outdoor.width()), LINE * 3)
-        feel_temperature.move(self._get_center_x(feel_temperature, self.region_outdoor.width()), LINE * 4)
-        humidity.move(self._get_center_x(humidity, self.region_outdoor.width()), LINE * 5)
-        wind_speed.move(self._get_center_x(wind_speed, self.region_outdoor.width()), LINE * 6)
-        wind_direction.move(self._get_center_x(wind_direction, self.region_outdoor.width()), LINE * 7)
-
-        wind = self._get_icon('infodisplay/ui/weather/res/wind.svg', self.region_outdoor)
-        wind.move(10,LINE * 9)
-        wind.show()
-
-
-
-    def updateForecastUI(self):
-        title = lblHeader("Forecast", self.region_forecast)
-        x = self._get_center_x(title, self.region_forecast.width())
-        title.move(x, 0)
-
-    def updateTimeDateUI(self):
-        title = lblHeader("Time & Date", self.region_time_date)
-        x = self._get_center_x(title, self.region_time_date.width())
-        title.move(x, 0)
-
-    def updateIndoorUI(self):
-        title = lblHeader("Indoor", self.region_indoor)
-        x = self._get_center_x(title, self.region_indoor.width())
-        title.move(x, 0)
+        self._update_current_weather()
+        self._update_weather_forecast()
 
     def _get_center_x(self, text, w):
         fm = QFontMetrics(text.font())
@@ -155,18 +56,13 @@ class Weather(QWidget):
             return
 
     def _update_current_weather(self):
-        filename = ConfigWeather.JSON_CURRENT_WEATHER
         logger.info(f'_update_current_weather')
+        self.current = get_dict_from_json_file(ConfigWeather.JSON_CURRENT_WEATHER)
+        self.outdoor.updateData(self.current)
 
     def _update_weather_forecast(self):
-        filename = ConfigWeather.JSON_WEATHER_FORECAST
         logger.info(f'_update_weather_forecast')
-
-    def testUI(self):
-        test = QLabel("Laber", self)
-        test.move(0, 0)
-        test.setText("alalalal\nkaksksk\tndndnd")
-        self.show()
+        self.forecast = get_dict_from_json_file(ConfigWeather.JSON_WEATHER_FORECAST)
 
     def createRegionOutdoor(self) -> QFrame:
         region_outdoor = QFrame(self)
@@ -209,17 +105,3 @@ class Weather(QWidget):
                                   ConfigWeather.REGION_INDOOR_WIDTH,
                                   ConfigWeather.REGION_INDOOR_HEIGHT)
         return region_indoor
-
-    def _get_weather_icon(self, id, region):
-        path = f'infodisplay/ui/weather/res/{id}.png'
-        label = lblText('', region)
-        pixmap = QPixmap(path)
-        label.setPixmap(pixmap)
-        return label
-
-    def _get_icon(self, path, region):
-        #path = f'infodisplay/ui/weather/res/{id}.png'
-        label = lblText('', region)
-        pixmap = QPixmap(path)
-        label.setPixmap(pixmap)
-        return label
--- infodisplay/ui/weather/weather_outdoor.py
+++ infodisplay/ui/weather/weather_outdoor.py
@@ -1,98 +1,121 @@
 # -*- coding: utf-8 -*-
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QWidget, QFrame, QVBoxLayout, QHBoxLayout
+from PyQt5.QtWidgets import QWidget, QApplication, QVBoxLayout, QPushButton, QLabel, QGridLayout
 from loguru import logger
 
 from configs.config_weather import lblText, lblHeader
 
+WI: int = int(1024 / 3)
+HI: int = int(600 / 4)
 
-class WeatherOutdoor(QWidget):
+SPACING: int = 0
+
+STRETCH_LABEL: int = 0
+STRETCH_BUTTON: int = 0
+
+MARGIN_LEFT: int = 0
+MARGIN_TOP: int = 0
+MARGIN_RIGHT: int = 0
+MARGIN_BOTTOM: int = 0
+
+
+class WeatherOutdooor(QWidget):
     def __init__(self, parent=None):
-        super(WeatherOutdoor, self).__init__(parent)
-        self.regionCurrent = self._set_currentRegion()
-        self.regionTemperature = self._set_temperatureRegion()
-        self.regionHumidity = self._setHumidityRegion()
+        super(WeatherOutdooor, self).__init__(parent)
+        if parent:
+            self.setGeometry(0, 0, parent.width(), parent.height())
+            self.setFixedSize(parent.width(), parent.height())
+        else:
+            self.setGeometry(100, 100, WI, HI)
+            self.setFixedSize(WI, HI)
+        self.lblHeader = lblHeader("Outdoor")
 
-    def currentData(self):
-        layout = QHBoxLayout(self.regionCurrent)
-        weather_icon_id = self.weather_data[0]['WeatherIcon']
-        weather_icon = self._get_weather_icon(weather_icon_id, self.regionCurrent)
-        weather_text_txt = self.weather_data[0]['WeatherText']
-        layout.addWidget(weather_icon)
-        layout.addStretch(1)
-        layout.addWidget(lblText(weather_text_txt))
-        layout.addStretch(2)
-        self.regionCurrent.setLayout(layout)
+        self.iconCurrent = lblText('')
+        self.iconCurrent.setPixmap(self._get_weather_icon('1', self))
 
-    def setHumidityData(self):
-        layout = QHBoxLayout(self.regionHumidity)
-        h_icon = self._get_icon('infodisplay/ui/weather/res/humidity.svg', self.regionHumidity)
-        h_text = str(self.weather_data[0]['RelativeHumidity']) + ' %'
-        layout.addWidget(h_icon)
-        layout.addStretch(1)
-        layout.addWidget(lblText(h_text))
-        layout.addStretch(2)
-        self.regionHumidity.setLayout(layout)
+        self.lblCurrent = lblText("Wolkig oder so")
 
-    def setTemperatureData(self):
-        layout = QHBoxLayout(self.regionTemperature)
-        temp_icon = self._get_icon('infodisplay/ui/weather/res/temperature.svg', self.regionTemperature)
-        c_icon = self._get_icon('infodisplay/ui/weather/res/C.svg', self.regionTemperature)
-        current_temp_txt = str(self.weather_data[0]['Temperature']['Metric']['Value'])
-        feel_temp_text = str(self.weather_data[0]['RealFeelTemperature']['Metric']['Value']) + ' Gefühlt'
-        # current_temp_txt = f'\t{current_temp_txt}\n{feel_temp_text}'
-        layout.addWidget(temp_icon)
-        layout.addStretch(1)
-        layout.addWidget(lblHeader(current_temp_txt))
-        layout.addStretch(1)
-        layout.addWidget(c_icon)
-        layout.addStretch(1)
-        # layout.setAlignment(Qt.AlignVCenter)
-        self.regionTemperature.setLayout(layout)
+        self.iconTemperature = self._get_icon(
+            '/home/thomaier/projects/myscreens/info-display/infodisplay/ui/weather/res/temperature.svg',
+            self)
+        self.lblTemperature = lblText("99,9")
+        self.iconTemperatureUnit = self._get_icon(
+            '/home/thomaier/projects/myscreens/info-display/infodisplay/ui/weather/res/C.svg', self)
 
-    def set_data(self, data: dict):
-        self.weather_data = data
-        self.currentData()
-        self.setTemperatureData()
-        self.setHumidityData()
+        self.iconHumidity = self._get_icon(
+            '/home/thomaier/projects/myscreens/info-display/infodisplay/ui/weather/res/humidity.svg', self)
+        self.lblHumidity = lblText("88")
+        self.lblHumidityUnit = lblText("%")
+
+        self.initUI()
+
+    def initUI(self):
+        layout = QGridLayout()
+        layout.setSpacing(0)
+        layout.setContentsMargins(10, 0, 0, 0)
+        # Header, center
+        layout.addWidget(self.lblHeader, 0, 0, 1, 6, Qt.AlignHCenter | Qt.AlignTop)
+        # Current Weather Icon and  Text
+        layout.addWidget(self.iconCurrent, 1, 0, Qt.AlignVCenter | Qt.AlignLeft)
+        layout.addWidget(self.lblCurrent, 1, 1, 1, 5, Qt.AlignVCenter | Qt.AlignLeft)
+        # Current Temperature
+        layout.addWidget(self.iconTemperature, 2, 0, Qt.AlignVCenter | Qt.AlignHCenter)
+        layout.addWidget(self.lblTemperature, 2, 1, Qt.AlignVCenter | Qt.AlignLeft)
+        layout.addWidget(self.iconTemperatureUnit, 2, 2, Qt.AlignVCenter | Qt.AlignLeft)
+
+        layout.addWidget(self.iconHumidity, 2, 3, Qt.AlignVCenter | Qt.AlignLeft)
+        layout.addWidget(self.lblHumidity, 2, 4, Qt.AlignVCenter | Qt.AlignLeft)
+        layout.addWidget(self.lblHumidityUnit, 2, 5, Qt.AlignVCenter | Qt.AlignLeft)
+
+        self.setLayout(layout)
+
+    def updateData(self, current):
+        logger.info(f'updateData')
+        self.iconCurrent.setPixmap(self._get_weather_icon(current[0]['WeatherIcon'], self))
+        self.lblCurrent.setText(current[0]['WeatherText'])
+        self.lblTemperature.setText(str(current[0]['RealFeelTemperature']['Metric']['Value']))
+        self.lblHumidity.setText(str(current[0]['RelativeHumidity']))
+
+    def TestinitUI(self):
+        label = QLabel("Label Text")
+        button = QPushButton("Button Text")
+
+        layout = QVBoxLayout()
+        layout.setSpacing(SPACING)
+
+        layout.setContentsMargins(MARGIN_LEFT,
+                                  MARGIN_TOP,
+                                  MARGIN_RIGHT,
+                                  MARGIN_BOTTOM)
+        layout.addWidget(label,
+                         STRETCH_LABEL,
+                         Qt.AlignTop)
+        layout.addWidget(button,
+                         STRETCH_BUTTON,
+                         Qt.AlignTop)
+
+        self.setLayout(layout)
+
+    ## Helper Functions ##
+    def _get_weather_icon(self, id, region):
+        path = f'infodisplay/ui/weather/res/{id}.png'
+        # path = f'/home/thomaier/projects/myscreens/info-display/infodisplay/ui/weather/res/{id}.png'
+        label = lblText('')
+        pixmap = QPixmap(path)
+        return pixmap
 
     def _get_icon(self, path, region):
         # path = f'infodisplay/ui/weather/res/{id}.png'
-        label = lblText('', region)
+        label = lblText('')
         pixmap = QPixmap(path)
-        #pixmap= pixmap.scaled(40,40, Qt.KeepAspectRatio)
+        # pixmap= pixmap.scaled(40,40, Qt.KeepAspectRatio)
         label.setPixmap(pixmap)
         return label
 
-    def _get_weather_icon(self, id, region):
-        path = f'infodisplay/ui/weather/res/{id}.png'
-        label = lblText('', region)
-        pixmap = QPixmap(path)
-        label.setPixmap(pixmap)
-        return label
 
-    def _set_currentRegion(self):
-        regionCurrent = QFrame(self)
-        regionCurrent.setGeometry(0, 0, self.parent().width(), 60)
-        regionCurrent.setObjectName("cur")
-        style = '#cur {border: 0px solid black; background-color: transparent;}'
-        regionCurrent.setStyleSheet(style)
-        return regionCurrent
-
-    def _set_temperatureRegion(self):
-        regionTemperature = QFrame(self)
-        regionTemperature.setGeometry(0, self.regionCurrent.height(), self.parent().width(), 80)
-        regionTemperature.setObjectName("rtemp")
-        style = '#rtemp {border: 0px solid black; background-color: transparent;}'
-        regionTemperature.setStyleSheet(style)
-        return regionTemperature
-
-    def _setHumidityRegion(self):
-        regionHumidity = QFrame(self)
-        regionHumidity.setGeometry(0, self.regionCurrent.height() + self.regionTemperature.height(),
-                                   self.parent().width(), 50)
-        regionHumidity.setObjectName("rhum")
-        style = '#rhum {border: 0px solid black; background-color: transparent;}'
-        regionHumidity.setStyleSheet(style)
-        return regionHumidity
+if __name__ == '__main__':
+    app = QApplication([])
+    w = WeatherOutdooor()
+    w.show()
+    app.exec_()
